db9fd38d88fddd32db1cddd821b3c031892a66d1
gauravluthraGL013 <gauravluthra061@gmail.com>
2021-04-09 12:14:19 +1200

    Added base code

---
+++ ClassExample/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassExample
+{
+  
+        class Person
+        {
+            // Member Variables
+            // method s
+
+            private string position { get; set; }
+            private int salary { get; set; }
+            private string department { get; set; }
+
+            // Methods
+
+            public void SetMember(string setposition, int setsalary, string setdepartment)
+            {
+                position = setposition;
+                salary = setsalary;
+                department = setdepartment;
+
+
+            }
+
+            public void GetMember(out string getposition, out int getsalary, out string getdepartment)
+            {
+                getposition = position;
+                getsalary = salary;
+                getdepartment = department;
+            }
+
+
+        }
+
+
+    }
+
+
---
+++ ClassExample/Program.cs
@@ -0,0 +1,48 @@
+﻿ using System;
+
+namespace ClassExample
+{
+    class Program
+    {
+        public static Person obj = new Person();
+        public static Person obj1 = new Person();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            Console.WriteLine("Please enter position ");
+            string position = Console.ReadLine();
+            Console.WriteLine("Please enter salary ");
+            string salary = Console.ReadLine();
+            Console.WriteLine("Please enter department");
+            string department = Console.ReadLine();
+
+            int newSalary = Convert.ToInt32(salary);
+
+            obj.SetMember(position, newSalary, department);
+
+            obj.GetMember(out position, out newSalary, out department);
+            Console.WriteLine("Position " + position);
+            Console.WriteLine(" Salary " + newSalary);
+            Console.WriteLine(" deaprtment " + department);
+
+
+            Console.WriteLine("Please enter position for Obj1 ");
+            position = Console.ReadLine();
+            Console.WriteLine("Please enter Salary Obj1");
+            salary = Console.ReadLine();
+            Console.WriteLine("Please enter department for obj1");
+            department = Console.ReadLine();
+
+            newSalary = Convert.ToInt32(salary);
+
+            obj1.SetMember(position, newSalary, department);
+            obj1.GetMember(out position, out newSalary, out department);
+            Console.WriteLine("Position: " + position);
+            Console.WriteLine(" Salary " + newSalary);
+            Console.WriteLine(" deaprtment " + department);
+        }
+    }
+}
+
